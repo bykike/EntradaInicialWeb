948987ff62efaaf39bfff91b7a1c7566834f5c56
Kike Algarra <kike@areavisual.net>
2019-07-29 17:04:12 +0200

    Modifcación del zoom del mapa.
    He variado el zoom para que sólo se vea el nombre de la empresa.

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+    (function($) {
+        "use strict";
+
+
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Es necesario escribir el nombre!",
+                    minlength: "Tu nombre debe de ser al menos de dos caracteres."
+                },
+                subject: {
+                    required: "Es necesario escribir en el asunto!",
+                    minlength: "El asunto debe de ser al menos de cuatro caracteres."
+                },
+                number: {
+                    required: "Este campo debe de llevar algo!",
+                    minlength: "Este campo debe de contener al menos cinco caracteres."
+                },
+                email: {
+                    required: "Sin email no hay mensaje."
+                },
+                message: {
+                    required: "Debe de escribir al menos algunas palabras en este mensaje.",
+                    minlength: "Debes de escribir algo más."
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+
+ })(jQuery)
+})
---
+++ js/index.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000000"
+    },
+    "button": {
+      "background": "#ffffff"
+    }
+  },
+  "content": {
+    "message": "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Utilizamos cookies propias y de terceros para el correcto funcionamiento del sitio Web e interactuar con redes sociales así como otras tecnologías similares, para mejorar su experiencia de navegación, analizar y medir su grado de compromiso con nuestro contenido. Si continúa navegando consideramos que acepta su uso.&nbsp;&nbsp;&nbsp;&nbsp;",
+    "dismiss": "Cerrar",
+    "link": "Leer más",
+    "href": "http://www.pritec.com/aviso-legal.html"
+  }
+})});
